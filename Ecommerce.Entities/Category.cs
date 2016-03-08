5998ddb374a1c2d6a09ba122693b1ad1cb2640bf
MattosV <vinicius.santos@lnnano.cnpem.br>
2016-03-08 14:44:19 -0300

    Creating Movie and Category Entities

---
+++ Ecommerce.Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int ExternalId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Ecommerce.Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce.Entities
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
