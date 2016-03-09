51dc637e975aaf16199347b88a10e32603fa967c
Oliviajun <olivia.miura@gmail.com>
2016-03-09 12:19:59 -0300

    Created PostScript for Add Users and Clients

--- Ecommerce.Database/Tables/Sales.sql
+++ Ecommerce.Database/Tables/Sales.sql
@@ -3,5 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [IdClient] INT NOT NULL, 
     [TotalPrice] NUMERIC(8, 2) NOT NULL, 
-    CONSTRAINT [FK_Sales_To_Client] FOREIGN KEY ([Idclient]) REFERENCES [Client]([Id])
+    CONSTRAINT [FK_Sales_To_Client] FOREIGN KEY ([IdClient]) REFERENCES [Client]([Id])
 )
---
+++ Ecommerce.Database/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Username] VARCHAR(20) NOT NULL, 
+    [Password] VARCHAR(20) NOT NULL, 
+    [IdClient] INT NULL, 
+    CONSTRAINT [FK_Users_To_Client] FOREIGN KEY ([IdClient]) REFERENCES [Client]([Id])
+)
