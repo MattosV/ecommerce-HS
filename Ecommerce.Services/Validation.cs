bd0bfeadeb0c4d82e7bab981347aa282f376a52c
Oliviajun <olivia.miura@gmail.com>
2016-03-14 10:35:15 -0300

    CPF type changed

--- Ecommerce.Services/Validation.cs
+++ Ecommerce.Services/Validation.cs
@@ -64,22 +64,16 @@ namespace Ecommerce.Services
             return true;
         }
 
-        //public static bool CheckBDate( DateTime Birthday)
-        //{
-        //    DateTime DateToday = DateTime.Today;
-        //    int Age = DateToday.Year - Birthday.Year;            
-            
-        //    if()
+        public static bool CheckBDate(DateTime Birthday) //Checks if the user is older than 16 and younger than 150 
+        {
+            DateTime DateToday = DateTime.Today;
+            int Age = DateToday.Year - Birthday.Year;
+            if (Birthday.Month > DateToday.Month) Age -= 1;
+            if (Birthday.Month == DateToday.Month || Birthday.Day > DateToday.Day) Age -= 1;
 
-         
-        //    return true;
-        //}
-
-
-
-
-
-
+            if (Age >= 16 || Age <= 150) return true;
+            return false;
+        }
 
     }
 }
