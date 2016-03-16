03f8a0e0ff712995be2cfbeb8e4d0c9cc1dc2620
Oliviajun <olivia.miura@gmail.com>
2016-03-16 09:55:34 -0300

    site master working database not working. :(

--- Ecommerce.Utils/Validation.cs
+++ Ecommerce.Utils/Validation.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-namespace Ecommerce.Utils
+﻿namespace Ecommerce.Utils
 {
     public static class Validation
     {
--- Ecommerce/App_Start/BundleConfig.cs
+++ Ecommerce/App_Start/BundleConfig.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Optimization;
-using System.Web.UI;
+﻿using System.Web.Optimization;
 
 namespace Ecommerce
 {
@@ -33,7 +28,6 @@ namespace Ecommerce
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                             "~/Scripts/modernizr-*"));
-
         }
     }
 }
